960bd01b34b82ae0ded3c06926ebb72d67e053fd
Larbo <mtoure@berrah.local>
2023-09-05 12:26:03 +0200

    init

---
+++ caller-service/src/main/java/pl/piomin/services/caller/CallerApplication.java
@@ -0,0 +1,23 @@
+package pl.piomin.services.caller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallerApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplateBuilder()
+//				.setReadTimeout(Duration.ofMillis(1000))
+				.build();
+	}
+	
+}
---
+++ callme-service/src/main/java/pl/piomin/services/callme/controller/CallmeController.java
@@ -0,0 +1,60 @@
+package pl.piomin.services.callme.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/callme")
+public class CallmeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallmeController.class);
+    private static final String INSTANCE_ID = UUID.randomUUID().toString();
+    private Random random = new Random();
+
+    @Autowired
+    Optional<BuildProperties> buildProperties;
+    @Value("${VERSION}")
+    private String version;
+
+    @GetMapping("/ping")
+    public String ping() {
+        LOGGER.info("Ping: name={}, version={}", buildProperties.isPresent() ? buildProperties.get().getName() : "callme-service", version);
+        return "I'm callme-service ------------- version-v________4667778888 ----------- " + version;
+    }
+
+    @GetMapping("/ping-with-random-error")
+    public ResponseEntity<String> pingWithRandomError() {
+        int r = random.nextInt(100);
+        if (r % 2 == 0) {
+            LOGGER.info("Ping with random error: name={}, version={}, random={}, httpCode={}",
+                    buildProperties.isPresent() ? buildProperties.get().getName() : "callme-service", version, r, HttpStatus.GATEWAY_TIMEOUT);
+            return new ResponseEntity<>("Surprise " + INSTANCE_ID + " " + version, HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            LOGGER.info("Ping with random error: name={}, version={}, random={}, httpCode={}",
+                    buildProperties.isPresent() ? buildProperties.get().getName() : "callme-service", version, r, HttpStatus.OK);
+            return new ResponseEntity<>("I'm callme-service" + INSTANCE_ID + " " + version, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/ping-with-random-delay")
+    public String pingWithRandomDelay() throws InterruptedException {
+        int r = new Random().nextInt(3000);
+        LOGGER.info("Ping with random delay: name={}, version={}, delay={}",
+                buildProperties.isPresent() ? buildProperties.get().getName() : "callme-service", version, r);
+        Thread.sleep(r);
+        return "I'm callme-service " + version;
+    }
+
+}
